884498106798a7e7fe95d2536d2e71c022a3b9ec
Kabkee <kabkee.moon@gmail.com>
2020-06-22 02:47:54 +0900

    complete

---
+++ crawling_meta.py
@@ -0,0 +1,210 @@
+[
+    {
+        "href": "/watch?v=8YVXtWmN4Oo&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=2&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E01MuddyPuddles | 中英字幕"
+    },
+    {
+        "href": "/watch?v=mpva7LPat4Q&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=3&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E02MrDinosaurIsLost | 中英字幕"
+    },
+    {
+        "href": "/watch?v=O3blmxLAU_8&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=4&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E03PollyParrot | 中英字幕"
+    },
+    {
+        "href": "/watch?v=E3Fl7Dp6P-Q&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=5&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E04BestFriend | 中英字幕"
+    },
+    {
+        "href": "/watch?v=BeW1jN9aelI&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=6&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E05HideAndSeek | 中英字幕"
+    },
+    {
+        "href": "/watch?v=xNQfL15OGRU&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=7&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E06ThePlaygroup | 中英字幕"
+    },
+    {
+        "href": "/watch?v=MDKpSj9P2Co&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=8&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E07MommyPigAtWork | 中英字幕"
+    },
+    {
+        "href": "/watch?v=TcwqpnGeNhU&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=9&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E08PiggyintheMiddle | 中英字幕"
+    },
+    {
+        "href": "/watch?v=O_IKH4g_EUo&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=10&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E09DaddyPigLosesGlasses | 中英字幕"
+    },
+    {
+        "href": "/watch?v=6MGWDUEsQmY&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=11&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E10Gardening | 中英字幕"
+    },
+    {
+        "href": "/watch?v=Kx3Sl4lmyr8&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=12&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E11Hiccups | 中英字幕"
+    },
+    {
+        "href": "/watch?v=MBLVqoat5lA&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=13&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E12Bicycles | 中英字幕"
+    },
+    {
+        "href": "/watch?v=yNTT8UxKN00&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=14&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E13Secrets | 中英字幕"
+    },
+    {
+        "href": "/watch?v=nhl6aif7Y14&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=15&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E14FlyingAKite | 中英字幕"
+    },
+    {
+        "href": "/watch?v=bIdKCOR6F3Q&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=16&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E15Picnic | 中英字幕"
+    },
+    {
+        "href": "/watch?v=gt7EX3lLz-I&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=17&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E16MusicalInstruments | 中英字幕"
+    },
+    {
+        "href": "/watch?v=xb1rptrRS2I&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=18&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E17FrogWormandButterfly | 中英字幕"
+    },
+    {
+        "href": "/watch?v=t7cskdC-9AM&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=19&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E18DressingUp | 中英字幕"
+    },
+    {
+        "href": "/watch?v=El6a-FEHc-Q&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=20&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E19NewShoes | 中英字幕"
+    },
+    {
+        "href": "/watch?v=3QQTBIoMX0c&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=21&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E20TheSchoolFete | 中英字幕"
+    },
+    {
+        "href": "/watch?v=gQZqgVhzTWw&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=22&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E21MommyPig'sBirthday | 中英字幕"
+    },
+    {
+        "href": "/watch?v=YawliyFOpts&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=23&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E22TheToothFairy | 中英字幕"
+    },
+    {
+        "href": "/watch?v=KUb1muh54Ug&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=24&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E23TheNewCar | 中英字幕"
+    },
+    {
+        "href": "/watch?v=vWIzSLrFWcc&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=25&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E24TreasureHunt | 中英字幕"
+    },
+    {
+        "href": "/watch?v=9cDHwOMG1I8&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=26&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E25NotVeryWell | 中英字幕"
+    },
+    {
+        "href": "/watch?v=kWUaMITnjyE&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=27&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E26Snow | 中英字幕"
+    },
+    {
+        "href": "/watch?v=RkO2JATqKks&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=28&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E27WindyCastle | 中英字幕"
+    },
+    {
+        "href": "/watch?v=IInhAXxmF-k&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=29&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E28MyCousinChloe | 中英字幕"
+    },
+    {
+        "href": "/watch?v=RfHqWg_7fEA&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=30&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E29Pancakes | 中英字幕"
+    },
+    {
+        "href": "/watch?v=uxwDookllbA&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=31&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E30Babysitting | 中英字幕"
+    },
+    {
+        "href": "/watch?v=OIJyYRMFkH4&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=32&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E31BalletLesson | 中英字幕"
+    },
+    {
+        "href": "/watch?v=wvGkNGHudt4&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=33&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E32Thunderstorm | 中英字幕"
+    },
+    {
+        "href": "/watch?v=nuYQ4BEAuyg&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=34&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E33CleaningtheCar | 中英字幕"
+    },
+    {
+        "href": "/watch?v=HvSVAD1rDqE&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=35&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E34Lunch | 中英字幕"
+    },
+    {
+        "href": "/watch?v=qwnB8ADumUQ&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=36&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E35Camping | 中英字幕"
+    },
+    {
+        "href": "/watch?v=qo8kEmUrlG8&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=37&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E36VeryHotDay | 中英字幕"
+    },
+    {
+        "href": "/watch?v=kylTifBJGws&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=38&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E37TheTreeHouse | 中英字幕"
+    },
+    {
+        "href": "/watch?v=uE8qkovLCOU&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=39&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E38FancyDressParty | 中英字幕"
+    },
+    {
+        "href": "/watch?v=nkq4c8X2Wlc&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=40&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E39TheMuseum | 中英字幕"
+    },
+    {
+        "href": "/watch?v=DNycZUBifwo&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=41&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E40SleepyPrincess | 中英字幕"
+    },
+    {
+        "href": "/watch?v=LomznphHqS0&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=42&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E41Chloe'sPuppetShow | 中英字幕"
+    },
+    {
+        "href": "/watch?v=yhqXaAZ13bw&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=43&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E42DaddyGetsFit | 中英字幕"
+    },
+    {
+        "href": "/watch?v=C-eZiL89tvM&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=44&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E43TidyingUp | 中英字幕"
+    },
+    {
+        "href": "/watch?v=CWL7ttQd07A&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=45&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E44Playground | 中英字幕"
+    },
+    {
+        "href": "/watch?v=Lq5ZPk_veqs&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=46&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E45DaddyPutsupaPicture | 中英字幕"
+    },
+    {
+        "href": "/watch?v=5iNDh8covWA&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=47&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E46AtTheBeach | 中英字幕"
+    },
+    {
+        "href": "/watch?v=cs1FKjNQp5g&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=48&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E47MrSkinnylegs | 中英字幕"
+    },
+    {
+        "href": "/watch?v=K3YoNzrUV34&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=49&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E48GrandpaPig'sBoat | 中英字幕"
+    },
+    {
+        "href": "/watch?v=s7dVVI94Qlo&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=50&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E49Shopping | 中英字幕"
+    },
+    {
+        "href": "/watch?v=ZqaWc1hHLvM&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=51&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E50MyBirthdayParty | 中英字幕"
+    },
+    {
+        "href": "/watch?v=GHdBmT65D8g&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=52&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E51Daddy'sMovie Camera | 中英字幕"
+    },
+    {
+        "href": "/watch?v=M2mfipbxgRc&list=PLRQqlqFCAa2A8Z4YpnljPy654lJp71JOb&index=53&t=0s",
+        "title": "小猪佩奇Peppa Pig | S01E52SchoolPlay | 中英字幕"
+    }
+]
